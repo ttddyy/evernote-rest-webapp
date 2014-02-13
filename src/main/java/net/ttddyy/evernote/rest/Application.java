e75f89683958c3fc98f180c07cb32c6d2830ee00
Tadaya Tsuyukubo <tadaya@ttddyy.net>
2014-02-12 16:31:18 -0800

    update SpringBootServletInitializer package due to the change in spring-boot
    SpringBootServletInitializer package has changed:
    https://github.com/spring-projects/spring-boot/commit/4a58171b5cd6aa5cad021f6e5c8636724eb86e27

--- src/main/java/net/ttddyy/evernote/rest/Application.java
+++ src/main/java/net/ttddyy/evernote/rest/Application.java
@@ -10,7 +10,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.SpringBootServletInitializer;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.*;
 import org.springframework.core.DefaultParameterNameDiscoverer;
 import org.springframework.core.ParameterNameDiscoverer;
